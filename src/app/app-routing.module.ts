5c68278dd9757ebefc3cdee2bd15c3f6e050537f
康营营 <52488837+yingying-huohua@users.noreply.github.com>
2020-10-24 09:36:18 +0800

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserProfitComponent} from './user/user-profit/user-profit.component';
+import {GroupProfitComponent} from './user/group-profit/group-profit.component';
+import {SymbolProfitComponent} from './symbol/symbol-profit/symbol-profit.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {SymbolAddComponent} from './setting/symbol-add/symbol-add.component';
+import {SymbolListComponent} from './symbol/symbol-list/symbol-list.component';
+
+
+const routes: Routes = [
+  {path: '',                redirectTo: '',  pathMatch: 'full'},
+  {path: 'dashboard',       component: DashboardComponent},
+  {path: 'symbol',          component: SymbolListComponent},
+  {path: 'symbol/profit',   component: SymbolProfitComponent},
+  {path: 'user/profit',     component: UserProfitComponent},
+  {path: 'group/profit',    component: GroupProfitComponent},
+  {path: 'setting',         loadChildren: () => import('./setting/setting.module').then(m => m.SettingModule)},
+  {path: 'symbol/add',      component: SymbolAddComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,65 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {NzLayoutModule} from 'ng-zorro-antd/layout';
+import {NzDropDownModule} from 'ng-zorro-antd/dropdown';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SymbolProfitComponent} from './symbol/symbol-profit/symbol-profit.component';
+import {UserProfitComponent} from './user/user-profit/user-profit.component';
+import {GroupProfitComponent} from './user/group-profit/group-profit.component';
+import {DashboardModule} from './dashboard/dashboard.module';
+import {SettingModule} from './setting/setting.module';
+import {NzTableModule} from 'ng-zorro-antd/table';
+import {NzDividerModule} from 'ng-zorro-antd/divider';
+import {SymbolAddComponent} from './setting/symbol-add/symbol-add.component';
+import {NzFormModule} from 'ng-zorro-antd/form';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {SymbolListComponent} from './symbol/symbol-list/symbol-list.component';
+import {LoginComponent} from './setting/login/login.component';
+import {NzModalModule} from 'ng-zorro-antd/modal';
+import {NzAutocompleteModule} from 'ng-zorro-antd/auto-complete';
+import {NzInputNumberModule} from 'ng-zorro-antd/input-number';
+import {NzDatePickerModule} from 'ng-zorro-antd/date-picker';
+import { UserProfitDetailComponent } from './user/user-profit-detail/user-profit-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SymbolListComponent,
+    SymbolProfitComponent,
+    UserProfitComponent,
+    GroupProfitComponent,
+    SymbolAddComponent,
+    LoginComponent,
+    UserProfitDetailComponent,
+  ],
+  imports: [
+    DashboardModule,
+    SettingModule,
+    BrowserModule,
+    AppRoutingModule,
+    NzLayoutModule,
+    NzDropDownModule,
+    BrowserAnimationsModule,
+    NzTableModule,
+    NzDividerModule,
+    NzFormModule,
+    NzButtonModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzModalModule,
+    FormsModule,
+    NzAutocompleteModule,
+    NzInputNumberModule,
+    NzDatePickerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
