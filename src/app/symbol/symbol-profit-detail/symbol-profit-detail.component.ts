873613ca7f595a1e87ce8abcd3e293751e6e6536
康营营 <yykang@huohuaschool.com>
2020-10-31 12:35:10 +0800

    add for: 品种列表及详情列表接口对接

--- src/app/symbol/symbol-profit-detail/symbol-profit-detail.component.ts
+++ src/app/symbol/symbol-profit-detail/symbol-profit-detail.component.ts
@@ -1,16 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {BaseComponent} from "../../BaseComponent";
+import {HttpService} from "../../../service/http/http.service";
 
 @Component({
   selector: 'app-symbol-profit-detail',
   templateUrl: './symbol-profit-detail.component.html',
   styleUrls: ['./symbol-profit-detail.component.css']
 })
-export class SymbolProfitDetailComponent implements OnInit {
-  sumbolDetailList = [];
-  isLoading = false
-  constructor() { }
-
-  ngOnInit(): void {
+export class SymbolProfitDetailComponent extends BaseComponent implements OnInit, OnChanges {
+  @Input() returnType; // 1:盈利， 2：亏损， 3：持平
+  @Input() symbol;
+  constructor(private httpService: HttpService) {
+    super();
   }
 
+  ngOnInit(): void {
+    this.initData();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (const key in changes) {
+      if (!changes.hasOwnProperty(key)) {
+        continue;
+      }
+
+      if (changes[key].firstChange) {
+        continue;
+      }
+
+      this.initData();
+    }
+  }
+
+  initData() {
+    const observer = {
+      next: response => {
+        this.isLoading = false;
+        this.dataList = response.result;
+        this.totalCount = response.count;
+      }
+    };
+    const object = {
+      returnType: this.returnType,
+      symbol: this.symbol,
+      sortField: this.sortField,
+      direction: this.direction,
+      openStart: this.openStart,
+      openEnd: this.openEnd,
+      pageNo: this.pageNo.toString(),
+      pageSize: this.pageSize.toString()
+    }
+    this.httpService.symbolProfitUserList(object).subscribe(observer);
+  }
 }
--- src/app/symbol/symbol-profit/symbol-profit.component.ts
+++ src/app/symbol/symbol-profit/symbol-profit.component.ts
@@ -1,78 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import {Config} from "../../../config/Config";
-import {NzTableQueryParams} from "ng-zorro-antd/table";
-import * as moment from "moment";
+import {Component, OnInit} from '@angular/core';
+import {BaseComponent} from "../../BaseComponent";
+import {HttpService} from "../../../service/http/http.service";
 
 @Component({
   selector: 'app-symbol-profit',
   templateUrl: './symbol-profit.component.html',
   styleUrls: ['./symbol-profit.component.css']
 })
-export class SymbolProfitComponent implements OnInit {
+export class SymbolProfitComponent extends BaseComponent implements OnInit {
   showDetail = false;
-  systemProfitList = [];
-  isLoading = false;
-
-  symbolInputValue = ''; // 品种输入框value
-  symbolSuggestionList = []; //品种搜索联想
-  date = null;
-  openStart = ''; // 开仓时间
-  openEnd = ''; // 关仓时间
-  sortField = Config.sortField; // 排序字段
-  direction = Config.direction; // 排序方式 desc|ase
-  pageNo    = Config.defaultPageNo;
-  pageSize  = Config.defaultPageSize;
-  totalCount = 0;
-
-  constructor() { }
+  currentSymbol;
+  currentReturnType;
+  constructor(private httpService: HttpService) {
+    super();
+  }
 
   ngOnInit(): void {
   }
 
   initData() {
+    const observer = {
+      next: response => {
+        this.isLoading = false;
+        this.dataList = response.result;
+        this.totalCount = response.count;
+      }
+    };
 
-  }
-
-  onPageIndexChange(pageNo) {
-
-  }
-
-  onQueryParamsChange(params: NzTableQueryParams) {
-    console.log(params);
-    const currentSort = params.sort.find(item => item.value !== null);
-    this.sortField = (currentSort && currentSort.key) || 'id';
-    const sortOrder = (currentSort && currentSort.value) || null;
-    this.direction = Config.ASC
-    if (!sortOrder || sortOrder === 'descend') {
-      this.direction = Config.DESC;
+    const object = {
+      symbol: this.symbolInputValue,
+      sortField: this.sortField,
+      direction: this.direction,
+      openStart: this.openStart,
+      openEnd: this.openEnd,
+      pageNo: this.pageNo.toString(),
+      pageSize: this.pageSize.toString()
     }
-
-    this.initData();
+    this.httpService.symbolProfitList(object).subscribe(observer);
   }
 
-  detail(item) {
+
+  detail(item, returnType) {
+    console.debug('查看详情');
+    this.currentSymbol = item.symbol;
+    this.currentReturnType = returnType;
     this.showDetail = true;
-  }
-
-  search(pageNo) {
-    this.showDetail = true;
-    this.pageNo = pageNo;
-    this.initData();
-  }
-
-  onSymbolInput(event: Event): void {
-    // 初始化联想
-
-  }
-
-  onChange(result: Date[]): void {
-    if (result.length === 0) {
-      this.openStart = '';
-      this.openEnd = '';
-      return;
-    }
-    this.openStart = moment(result[0]).format('YYYY-MM-DD');
-    this.openEnd = moment(result[1]).format('YYYY-MM-DD');
-    // console.debug('From: ',  this.openStart, ', to: ',  this.openEnd);
   }
 }
