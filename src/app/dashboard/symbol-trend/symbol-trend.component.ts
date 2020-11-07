1647d5bd0dea16f741fb19e35223add973d9cbe3
康营营 <yykang@huohuaschool.com>
2020-11-07 11:32:09 +0800

    add for: 增加品种趋势页面

--- src/app/dashboard/symbol-trend/symbol-trend.component.ts
+++ src/app/dashboard/symbol-trend/symbol-trend.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./symbol-trend.component.css']
 })
 export class SymbolTrendComponent implements OnInit {
-
+  componentActive;
   constructor() { }
 
   ngOnInit(): void {
+    this.initData()
   }
 
+  initData() {
+    this.componentActive = {
+      textStyle: {
+        fontSize: '10px',
+        color: '#B1B0B0'
+      },
+      tooltip: {
+        show: true,
+        trigger: 'axis',
+        // axisPointer: {
+        //   type: 'shadow'
+        // },
+        transitionDuration: 0
+      },
+      xAxis: {
+        type: 'category',
+        data: ['2020-08-03', '2020-08-04', '2020-08-05', '2020-08-06', '2020-08-07', '2020-08-08', '2020-08-09', '2020-08-10']
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {
+          lineStyle: {color: 'rgb(51,51,51)'}
+        }
+      },
+      series: [{
+        data: [12,16,17,19,25,63,79],
+        type: 'line',
+        lineStyle: {
+          color: '#FE5702'
+        }
+      }]
+    }
+  }
 }
