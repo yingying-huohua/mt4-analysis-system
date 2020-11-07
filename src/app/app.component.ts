5c017ff75a5fa20b782fb4310109825ae9973e9d
康营营 <yykang@huohuaschool.com>
2020-11-07 10:48:08 +0800

    add for: 顶部菜单数据动态加载，绑定顶部菜单事件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {registerLocaleData} from "@angular/common";
 import zh from "@angular/common/locales/zh";
+import {HeaderMenu} from "../constant/HeaderMenu";
+import {Router} from "@angular/router";
 registerLocaleData(zh)
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'md4-analysis-system';
+  headerMenu = [];
+  selectedId = HeaderMenu.data;
+  showLeftPanel = false;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.initHeaderMenu();
+    this.renderTab(this.selectedId)
+  }
+
+  /**
+   * 渲染tab
+   * @param item
+   */
+  renderTab(id) {
+    this.selectedId = id;
+    switch (id) {
+      // case HeaderMenu.total:
+      //   break;
+      case HeaderMenu.data:
+        this.showLeftPanel = true;
+        this.router.navigate(['./']);
+        break;
+      case HeaderMenu.symbol:
+        this.showLeftPanel = false;
+        this.router.navigate(['./dashboard']);
+        break;
+    }
+  }
+
+  /**
+   * 初始化顶部菜单
+   */
+  private initHeaderMenu() {
+    this.headerMenu = [
+      // {
+      //   id: HeaderMenu.total,
+      //   title: '综合看板'
+      // },
+      {
+        id: HeaderMenu.symbol,
+        title: '品种看板'
+      },
+      {
+        id: HeaderMenu.data,
+        title: '数据分析'
+      }
+
+    ]
+  }
 }
---
+++ src/constant/HeaderMenu.ts
@@ -0,0 +1,10 @@
+/**
+ * 头部菜单
+ *@author kyy
+ *@date 2020/11/7 10:28
+ */
+export const HeaderMenu = {
+  total: 'total',
+  symbol: 'symbol',
+  data: 'data'
+}
